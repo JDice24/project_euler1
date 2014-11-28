76d4c1791639c4611e685df7dbe48c3568efb778
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-11-28 11:39:26 +0000

    multiple of 5 tests passing

--- spec/euler1_spec.rb
+++ spec/euler1_spec.rb
@@ -10,4 +10,12 @@ describe 'euler1' do
     expect(is_multiple_of_three?(2)).to be false
   end
 
+  it 'should know that 5 is a multiple of 5' do
+    expect(is_multiple_of_five?(5)).to be true
+  end
+
+  it 'should know that 4 is not a multiple of 5' do
+    expect(is_multiple_of_five?(2)).to be false
+  end
+
 end
